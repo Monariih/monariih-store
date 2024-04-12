6fea640fef76700308338f4c19dde580850fa011
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-12 19:29:20 -0300

    create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() { 
+  return (
+		<footer className="absolute my-2 mx-auto bottom-0">
+			<div>
+				<p className="text-center dark:text-gray-500">
+					© 2021 Monariih Store. All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
+}
