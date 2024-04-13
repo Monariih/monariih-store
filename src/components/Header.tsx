1d2f03b6111d66a2103e046836f85199a671d8e4
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 17:23:49 -0300

    remove useless letters

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
 	        <div className="flex items-center lg:order-2">
 	          <a href="" className="bg-white flex hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
 	            <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24"><path d="M24,3H4.242L4.2,2.649A3,3,0,0,0,1.222,0H0V2H1.222a1,1,0,0,1,.993.883L3.8,16.351A3,3,0,0,0,6.778,19H20V17H6.778a1,1,0,0,1-.993-.884L5.654,15H21.836Z" /><circle cx="7" cy="22" r="2" /><circle cx="17" cy="22" r="2" /></svg>
-	            <span className="hidden lg:inline">//NumeroCompras</span>
+	            <span className="hidden lg:inline">NumeroCompras</span>
 	          </a>
 	        </div>
 	      </div>
