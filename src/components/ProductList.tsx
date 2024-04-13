18361652cf091ffc4ac9c776e00631bdc5bf5f21
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 15:18:02 -0300

    create product list component
    this also moves all the content that was in a father component to it, this was made for clean code reasons

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+export default function Home() {
+
+  const getProducts = async () => {
+    const response = await fetch("https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC");
+    const data = await response.json();
+    console.log(data);
+    }
+
+  return (
+    <main className=" bg-gray-400">
+      <div className="m-16 grid grid-cols-4 gap-4">
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          31
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          02
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          03
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          04
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          05
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          06
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          07
+        </div>
+        <div className="self-center text-center m-2 rounded-md bg-white">
+          08
+        </div>
+      </div>
+    </main>
+  );
+}
