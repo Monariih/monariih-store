82012b6093edfde27fb2726ecda3aec64e216aff
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 15:31:21 -0300

    add on load function to get products

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,16 +2,20 @@
 export default function Home() {
 
   const getProducts = async () => {
-    const response = await fetch("https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC");
+  	const response = await fetch("https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC");
     const data = await response.json();
     console.log(data);
-    }
+	}
+	
+	onload = () => {
+		getProducts();
+	}
 
   return (
     <main className=" bg-gray-400">
       <div className="m-16 grid grid-cols-4 gap-4">
         <div className="self-center text-center m-2 rounded-md bg-white">
-          31
+          01
         </div>
         <div className="self-center text-center m-2 rounded-md bg-white">
           02
