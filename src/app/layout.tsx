8ef663901bf2a2fc8824c0e3420362b8cb6d79fe
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 15:31:35 -0300

    add needed imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ProductList from "@/components/ProductList";
 
 const inter = Inter({ subsets: ["latin"] });
 
