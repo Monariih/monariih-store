8a3b9583e5411e38d9298d6988dedb833a7e427a
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 17:52:27 -0300

    declare children to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
+        {children}
         <Header />
           <ProductList />
         <Footer />
-      </body>  
+      </body> 
     </html>
   );
 }
