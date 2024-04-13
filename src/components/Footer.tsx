f0370f4520f670ad41874bc77a970c49c0e87b52
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 15:20:08 -0300

    add tailwind classes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() { 
   return (
-		<footer className="absolute my-2 mx-auto bottom-0">
+		<footer className="absolute w-full bg-footerGray mt-2 mx-auto bottom-0">
 			<div>
 				<p className="text-center my-2">
 					MKS sistemas &copy; Todos os direitos reservados
