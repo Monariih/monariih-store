53c334d5370e608b22847b96ab7257a321fb579d
Lucas Monari <lucas.monari.lm@outlook.com>
2024-04-13 15:19:49 -0300

    rename footer properties

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
 		<footer className="absolute my-2 mx-auto bottom-0">
 			<div>
-				<p className="text-center dark:text-gray-500">
-					© 2021 Monariih Store. All rights reserved.
+				<p className="text-center my-2">
+					MKS sistemas &copy; Todos os direitos reservados
 				</p>
 			</div>
 		</footer>
